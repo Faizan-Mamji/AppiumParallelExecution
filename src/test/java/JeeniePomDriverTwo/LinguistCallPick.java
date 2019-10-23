ab64c0ce52548ff97bce001e32528b09a2afda0e
faizan.mamji <faizan.mamji@10pearls.local>
2019-10-23 11:54:08 +0500

    Adding readme

---
+++ src/test/java/JeeniePomDriverTwo/LinguistCallPick.java
@@ -0,0 +1,17 @@
+package JeeniePomDriverTwo;
+
+import DriverAppium.MainDriverCalling;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import java.util.List;
+
+public class LinguistCallPick extends MainDriverCalling {
+    public LinguistCallPick(AndroidDriver<MobileElement> driver) {
+        this.driverTwo = driver;
+    }
+
+    public List<MobileElement> callPickingLinguist() {
+        return driverTwo.findElementsByXPath("//android.widget.TextView[@index='0']");
+    }
+}
